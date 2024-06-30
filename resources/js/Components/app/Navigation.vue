99a45ec4963857183cb3e2936bc235ee4b7b1e58
benhelal <benhelalwajdi@gmail.com>
2024-06-30 15:51:10 -0400

    done

---
+++ resources/js/Components/app/Navigation.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        Navigation
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+       Search form
+    </div>
+</template>
+
+<style scoped>
+
+</style>
